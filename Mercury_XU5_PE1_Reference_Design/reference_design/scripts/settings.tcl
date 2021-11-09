c74d8e3f71355eaba791bf24b0ed5f713ebb229b
Kento Okazaki <kento.okazaki@cern.ch>
2021-11-09 17:48:18 +0900

    sync

--- Mercury_XU5_PE1_Reference_Design/reference_design/scripts/settings.tcl
+++ Mercury_XU5_PE1_Reference_Design/reference_design/scripts/settings.tcl
@@ -34,7 +34,7 @@
 
 # ----------------------------------------------------------------------------------
 # Modify this variable to select your module
-if {![info exists module_name]} {set module_name ME-XU5-5EV-2I-D12E}
+if {![info exists module_name]} {set module_name ME-XU5-2CG-1E-D10H}
 if {![info exists baseboard]}   {set baseboard PE1}
 # ----------------------------------------------------------------------------------
 
@@ -48,90 +48,80 @@ if {[lindex $argv 0] != ""} { set module_name [lindex $argv 0] }
 set module Mercury_XU5
 
 switch $module_name {
-    ME-XU5-2CG-1E-D10H {
-        set part xczu2cg-sfvc784-1-e
-        set ETH_PHASE ETH_PHASE_76_5
-        set PS_DDR PS_D10H
-        set PL_DDR PL_1_D10H
-        set MGT_routing G1
-    }
-    ME-XU5-2EG-1I-D11E {
-        set part xczu2eg-sfvc784-1-i
-        set ETH_PHASE ETH_PHASE_76_5
-        set PS_DDR PS_D11E
-        set PL_DDR PL_1_D10H
-        set MGT_routing G1
-    }
-    ME-XU5-3EG-2I-D11E {
-        set part xczu3eg-sfvc784-2-i
-        set ETH_PHASE ETH_PHASE_76_5
-        set PS_DDR PS_D11E
-        set PL_DDR PL_2_D11E
-        set MGT_routing G1
-    }
-    ME-XU5-4CG-1E-D11E-G1 {
-        set part xczu4cg-sfvc784-1-e
-        set ETH_PHASE ETH_PHASE_90
-        set PS_DDR PS_D11E
-        set PL_DDR PL_1_D10H
-        set MGT_routing G1
-    }
-    ME-XU5-4EV-1I-D11E {
-        set part xczu4ev-sfvc784-1-i
-        set ETH_PHASE ETH_PHASE_90
-        set PS_DDR PS_D11E
-        set PL_DDR PL_1_D10H
-        set MGT_routing None
-    }
-    ME-XU5-4EV-1I-D11E-G1 {
-        set part xczu4ev-sfvc784-1-i
-        set ETH_PHASE ETH_PHASE_90
-        set PS_DDR PS_D11E
-        set PL_DDR PL_1_D10H
-        set MGT_routing G1
-    }
-    ME-XU5-5EV-2I-D12E {
-        set part xczu5ev-sfvc784-2-i
-        set ETH_PHASE ETH_PHASE_90
-        set PS_DDR PS_D12E
-        set PL_DDR PL_2_D12E
-        set MGT_routing None
-    }
-    ME-XU5-5EV-1E-D11E {
-        set part xczu5ev-sfvc784-1-e
-        set ETH_PHASE ETH_PHASE_90
-        set PS_DDR PS_D11E
-        set PL_DDR PL_1_D10H
-        set MGT_routing None
-    }
-    ME-XU5-5EV-2I-D12E-G1 {
-        set part xczu5ev-sfvc784-2-i
-        set ETH_PHASE ETH_PHASE_90
-        set PS_DDR PS_D12E
-        set PL_DDR PL_2_D12E
-        set MGT_routing G1
-    }
-    ME-XU5-5EV-3E-D13E {
-        set part xczu5ev-sfvc784-3-e
-        set ETH_PHASE ETH_PHASE_90
-        set PS_DDR PS_D13E
-        set PL_DDR PL_2_D13E
-        set MGT_routing None
-    }
-    default {
-        puts "$module_name not available"
-        break
-    }
+  ME-XU5-2CG-1E-D10H {
+    set part xczu2cg-sfvc784-1-e 
+    set PS_DDR PS_D10H
+    set PL_DDR PL_1_D10H
+    set MGT_routing G1
+  }
+  ME-XU5-2EG-1I-D11E {
+    set part xczu2eg-sfvc784-1-i 
+    set PS_DDR PS_D11E
+    set PL_DDR PL_1_D10H
+    set MGT_routing G1
+  }
+  ME-XU5-3EG-2I-D11E {
+    set part xczu3eg-sfvc784-2-i 
+    set PS_DDR PS_D11E
+    set PL_DDR PL_2_D11E
+    set MGT_routing G1
+  }
+  ME-XU5-4CG-1E-D11E-G1 {
+    set part xczu4cg-sfvc784-1-e 
+    set PS_DDR PS_D11E
+    set PL_DDR PL_1_D10H
+    set MGT_routing G1
+  }
+  ME-XU5-4EV-1I-D11E {
+    set part xczu4ev-sfvc784-1-i 
+    set PS_DDR PS_D11E
+    set PL_DDR PL_1_D10H
+    set MGT_routing None
+  }
+  ME-XU5-4EV-1I-D11E-G1 {
+    set part xczu4ev-sfvc784-1-i 
+    set PS_DDR PS_D11E
+    set PL_DDR PL_1_D10H
+    set MGT_routing G1
+  }
+  ME-XU5-5EV-2I-D12E {
+    set part xczu5ev-sfvc784-2-i 
+    set PS_DDR PS_D12E
+    set PL_DDR PL_2_D12E
+    set MGT_routing None
+  }
+  ME-XU5-5EV-1E-D11E {
+    set part xczu5ev-sfvc784-1-e 
+    set PS_DDR PS_D11E
+    set PL_DDR PL_1_D10H
+    set MGT_routing None
+  }
+  ME-XU5-5EV-2I-D12E-G1 {
+    set part xczu5ev-sfvc784-2-i 
+    set PS_DDR PS_D12E
+    set PL_DDR PL_2_D12E
+    set MGT_routing G1
+  }
+  ME-XU5-5EV-3E-D13E {
+    set part xczu5ev-sfvc784-3-e 
+    set PS_DDR PS_D13E
+    set PL_DDR PL_2_D13E
+    set MGT_routing None
+  }
+  default {
+    puts "$module_name not available"
+    break
+  }
 }
 
 #create project name for design
 if {![info exists project_name]} {
-    set project_name ${module}
-    if {[info exists baseboard]} {
-        lappend project_name ${baseboard}
-        puts $project_name
-    }
-    set project_name [string map {" " "_"} "${project_name}"]
+  set project_name ${module}
+  if {[info exists baseboard]} {
+    lappend project_name ${baseboard}
+    puts $project_name
+  }
+  set project_name [string map {" " "_"} "${project_name}"]
 }
 
 puts "INFO: settings.tcl file loaded."
