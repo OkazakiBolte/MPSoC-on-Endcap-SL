1107c58051d2d6595995512d4f681157deed576b
Kento Okazaki <kento.okazaki@cern.ch>
2021-11-16 13:34:52 +0900

    generate refdes' bootfiles

--- Mercury_XU5_PE1_Reference_Design/reference_design/scripts/settings.tcl
+++ Mercury_XU5_PE1_Reference_Design/reference_design/scripts/settings.tcl
@@ -34,7 +34,7 @@
 
 # ----------------------------------------------------------------------------------
 # Modify this variable to select your module
-if {![info exists module_name]} {set module_name ME-XU5-2CG-1E-D10H}
+if {![info exists module_name]} {set module_name ME-XU5-5EV-2I-D12E}
 if {![info exists baseboard]}   {set baseboard PE1}
 # ----------------------------------------------------------------------------------
 
@@ -49,61 +49,61 @@ set module Mercury_XU5
 
 switch $module_name {
   ME-XU5-2CG-1E-D10H {
-    set part xczu2cg-sfvc784-1-e 
+    set part xczu2cg-sfvc784-1-e
     set PS_DDR PS_D10H
     set PL_DDR PL_1_D10H
     set MGT_routing G1
   }
   ME-XU5-2EG-1I-D11E {
-    set part xczu2eg-sfvc784-1-i 
+    set part xczu2eg-sfvc784-1-i
     set PS_DDR PS_D11E
     set PL_DDR PL_1_D10H
     set MGT_routing G1
   }
   ME-XU5-3EG-2I-D11E {
-    set part xczu3eg-sfvc784-2-i 
+    set part xczu3eg-sfvc784-2-i
     set PS_DDR PS_D11E
     set PL_DDR PL_2_D11E
     set MGT_routing G1
   }
   ME-XU5-4CG-1E-D11E-G1 {
-    set part xczu4cg-sfvc784-1-e 
+    set part xczu4cg-sfvc784-1-e
     set PS_DDR PS_D11E
     set PL_DDR PL_1_D10H
     set MGT_routing G1
   }
   ME-XU5-4EV-1I-D11E {
-    set part xczu4ev-sfvc784-1-i 
+    set part xczu4ev-sfvc784-1-i
     set PS_DDR PS_D11E
     set PL_DDR PL_1_D10H
     set MGT_routing None
   }
   ME-XU5-4EV-1I-D11E-G1 {
-    set part xczu4ev-sfvc784-1-i 
+    set part xczu4ev-sfvc784-1-i
     set PS_DDR PS_D11E
     set PL_DDR PL_1_D10H
     set MGT_routing G1
   }
   ME-XU5-5EV-2I-D12E {
-    set part xczu5ev-sfvc784-2-i 
+    set part xczu5ev-sfvc784-2-i
     set PS_DDR PS_D12E
     set PL_DDR PL_2_D12E
     set MGT_routing None
   }
   ME-XU5-5EV-1E-D11E {
-    set part xczu5ev-sfvc784-1-e 
+    set part xczu5ev-sfvc784-1-e
     set PS_DDR PS_D11E
     set PL_DDR PL_1_D10H
     set MGT_routing None
   }
   ME-XU5-5EV-2I-D12E-G1 {
-    set part xczu5ev-sfvc784-2-i 
+    set part xczu5ev-sfvc784-2-i
     set PS_DDR PS_D12E
     set PL_DDR PL_2_D12E
     set MGT_routing G1
   }
   ME-XU5-5EV-3E-D13E {
-    set part xczu5ev-sfvc784-3-e 
+    set part xczu5ev-sfvc784-3-e
     set PS_DDR PS_D13E
     set PL_DDR PL_2_D13E
     set MGT_routing None
